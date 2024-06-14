3f413d8e98b6f56ee29e61a6bb858e98cc02f968
tgnetok <vera.iva120608@gmail.com>
2024-06-14 13:34:42 +0000

    mememe

---
+++ lec_1_data_type.py
@@ -0,0 +1,20 @@
+type (3)
+
+print (type(3))
+
+print (type(3.4))
+
+print (type("BDSM"))
+
+a = "BDSM"
+print (a)
+
+a = ["BDSM", 1, "sex", 6, 8]
+print (type(a))
+
+print (type(True))
+
+print (type(False))
+
+print (type(None))
+print (print(3))
---
+++ lec_1_input.py
@@ -0,0 +1,8 @@
+# a = input ()
+# print (a)
+
+a = input ("who are you:")
+print (a)
+print (type(a))
+
+a = int(input("who are you:"))
