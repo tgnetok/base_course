d1677885b8868400899b95f772c4cb510d3479e8
tgnetok <vera.iva120608@gmail.com>
2024-06-15 12:09:27 +0000

    yes

---
+++ smertelniy_file.py
@@ -0,0 +1,7 @@
+EbatTebyaVRot = []
+
+EbatTebyaVRot.append (input("Ты долбоеб? "))
+EbatTebyaVRot.append (input("Ты гей? "))
+EbatTebyaVRot.append (int(input("Сколько у тебя см? ")))
+if EbatTebyaVRot == ['да', 'конечно', 228] : print ("ЕБАААААААТЬ")
+else : print ("Ну ты и чмо.")
