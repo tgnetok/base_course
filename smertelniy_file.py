73b52c26762db520d65fa4281d93137d63ab342a
tgnetok <vera.iva120608@gmail.com>
2024-06-15 12:18:46 +0000

    ?????

--- smertelniy_file.py
+++ smertelniy_file.py
@@ -5,3 +5,5 @@ EbatTebyaVRot.append (input("Ты гей? "))
 EbatTebyaVRot.append (int(input("Сколько у тебя см? ")))
 if EbatTebyaVRot == ['да', 'конечно', 228] : print ("ЕБАААААААТЬ")
 else : print ("Ну ты и чмо.")
+
+# кто прочитал, тот пидорас. 
