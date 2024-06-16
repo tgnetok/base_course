19875ee90efbaad8519291f66761b1a42bd38ad4
tgnetok <vera.iva120608@gmail.com>
2024-06-16 12:23:36 +0000

    ))))

--- smertelniy_file.py
+++ smertelniy_file.py
@@ -10,13 +10,18 @@ if EbatTebyaVRot == ['да', 'конечно', 228] : print ("ЕБААААААА
 # ну пиздец.
 
 BDSM = "Ты тупой, ебать."
+Sex = "Шо, 200 IQ?!"
 if EbatTebyaVRot[0] == "да" or "yes" or "конечно" or "ага" : print (BDSM)
+else : print (Sex)
 
 GayMaster = "Хахахаха, ты гей)))"
+NotGay = "Не верю, пидор."
 if EbatTebyaVRot[1] == "да" or "конечно" or "yes" or "ага" : print (GayMaster)
-print (GayMaster)
+else : print (NotGay)
 
 KorotkiyHui = "Пиздец..."
+BigHui = "Заебись!"
 if EbatTebyaVRot[2] < 30 : print (KorotkiyHui)
+else : print (BigHui)
 
 # это полный пиздец.
