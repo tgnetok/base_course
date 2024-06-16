7647d1e692917030ae4748ace0f684a19016f27c
tgnetok <vera.iva120608@gmail.com>
2024-06-16 09:35:38 +0000

    не работает (((

--- smertelniy_file.py
+++ smertelniy_file.py
@@ -7,8 +7,14 @@ if EbatTebyaVRot == ['да', 'конечно', 228] : print ("ЕБААААААА
 
 # кто прочитал, тот пидорас. 
 
-if EbatTebyaVRot[0] == "да" : EbatTebyaVRot = BDSM
-BDSM = ("Ты тупой, ебать.")
+BDSM = "Ты тупой, ебать."
+if EbatTebyaVRot[0] == "да" or "yes" or "конечно" or "ага" : EbatTebyaVRot = BDSM
 print (BDSM)
 
-if EbatTebyaVRot[1] = ""
+GayMaster = "Хахахаха, ты гей)))"
+if EbatTebyaVRot[1] == "да" or "конечно" or "yes" or "ага" : EbatTebyaVRot = GayMaster
+print (GayMaster)
+
+KorotkiyHui = "Микрочлен!"
+if int(EbatTebyaVRot[2]) < 30 : EbatTebyaVRot = KorotkiyHui
+print (KorotkiyHui)
