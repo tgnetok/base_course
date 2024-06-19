032e44a844c2f52ff9ba9e4b93fddea695b246bd
tgnetok <vera.iva120608@gmail.com>
2024-06-19 10:29:57 +0000

    yamashushopu

--- smertelniy_file.py
+++ smertelniy_file.py
@@ -1,31 +1,55 @@
 EbatTebyaVRot = []
 
 EbatTebyaVRot.append (input("Ты долбоеб? "))
-EbatTebyaVRot.append (input("Ты гей? "))
-EbatTebyaVRot.append (int(input("Сколько у тебя см? ")))
-if EbatTebyaVRot == ['да', 'да', 228] : print ("ЕБАААААААТЬ")
-
-# кто прочитал, то пидор.
-# я тоже пидор.
-# ну пиздец.
 
 BDSM = "Ты тупой, ебать."
 Sex = "Шо, 200 IQ?!"
 Dolboeb = "Ответь да или нет, сука!"
-if EbatTebyaVRot[0] == "да" : print (BDSM)
-elif EbatTebyaVRot[0] == "нет" : print (Sex)
-else : print (Dolboeb)
+if EbatTebyaVRot[0] == "да" : 
+    print (BDSM)
+elif EbatTebyaVRot[0] == "нет" : 
+    print (Sex)
+else : 
+    print (Dolboeb)
+
+# я не долбоёб))))))
+# а ты - да.
+
+EbatTebyaVRot.append (input("Ты гей? "))
 
 GayMaster = "Хахахаха, ты гей)))"
 NotGay = "Не верю, пидор."
 Debil = "БЫЛ ПРОСТОЙ ВОПРОС ДА ИЛИ НЕТ."
-if EbatTebyaVRot[1] == "да" : print (GayMaster)
-elif EbatTebyaVRot[1] == "нет" : print (NotGay)
-else: print (Debil)
+if EbatTebyaVRot[1] == "да" : 
+    print (GayMaster)
+elif EbatTebyaVRot[1] == "нет" : 
+    print (NotGay)
+else: 
+    print (Debil)
+
+# и я не гей.
+# а ты пидор ахахахаха)))))
+
+EbatTebyaVRot.append (int(input("Сколько у тебя см? ")))
 
 KorotkiyHui = "Пиздец..."
 BigHui = "Заебись!"
-if EbatTebyaVRot[2] < 30 : print (KorotkiyHui)
-elif EbatTebyaVRot[2] >= 30 : print (BigHui)
+if EbatTebyaVRot[2] < 30 : 
+    print (KorotkiyHui)
+elif EbatTebyaVRot[2] >= 30 : 
+    print (BigHui)
 
-# это полный пиздец.
+# у меня 69 км и 228 см.
+# а у тебя короткий.
+
+if EbatTebyaVRot == ['да', 'да', 228] : 
+    print ("ЕБАААААААТЬ")
+
+# ЕБААААААААТЬ
+
+'''
+Я мажу жопу вазелином,
+Я так люблю анальный секс...
+Когда мужчина спит с мужчиной,
+Мне симпатичен сей процесс.
+'''
