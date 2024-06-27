8c71a37ca56024cdd66bf9e1098334db2116a589
tgnetok <vera.iva120608@gmail.com>
2024-06-27 17:02:25 +0000

    ttttt

---
+++ lab_4_task_2.py
@@ -0,0 +1,10 @@
+import numpy as baigashov
+
+arg_numbers_list = [1, 2, 3, 4]
+arg_numbers_array = baigashov.array (arg_numbers_list)
+
+def arr_func (arg_numbers_array) :
+    multiply = arg_numbers_array[0] * arg_numbers_array[1] * arg_numbers_array[2] * arg_numbers_array[3]
+    return multiply
+
+print (arr_func (arg_numbers_array))
