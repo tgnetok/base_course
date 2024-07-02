6410db849adf11d378105f267e721d5867f72d9b
tgnetok <vera.iva120608@gmail.com>
2024-07-02 13:25:55 +0000

    yay

--- lab_5_task_2.py
+++ lab_5_task_2.py
@@ -8,3 +8,14 @@ name_list = []
 for symbol in name_up:
     name_list.append (ord(symbol))
 print (name_list)
+print (max(name_list))
+
+name_name = "_".join(name)
+name_low = name_name.lower ()
+
+name_list = []
+
+for symbol in name_low:
+    name_list.append (ord(symbol))
+print (name_list)
+print (max(name_list))
