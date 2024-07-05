9877a36d054237825e627cfda2e23ea2e39835b8
tgnetok <vera.iva120608@gmail.com>
2024-07-05 12:32:34 +0000

    lsuhgklsluhvbksjnkuhn

--- lab_5_task_1.py
+++ lab_5_task_1.py
@@ -1,8 +1,21 @@
 import numpy as baigashov
+import random
 
-list_one = [1, 45, 75, 8, 4, 12]
-list_two = [5, 6, 34, 38, 2]
-list_three = [23, 94, 25, 4, 8, 36]
+N = int(input('Введите длину массивов: '))
+
+list_one = []
+list_two = []
+list_three = []
+
+for i in range (0, N, 1) :
+    values_one = random.randint (0, 100)
+    list_one.append (values_one)
+for i in range (0, N, 1) :
+    values_two = random.randint (0, 100)
+    list_two.append (values_two)
+for i in range (0, N, 1) :
+    values_three = random.randint (0, 100)
+    list_three.append (values_three)
 
 array_one = baigashov.array (list_one)
 array_two = baigashov.array (list_two)
