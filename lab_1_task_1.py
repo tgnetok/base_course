a03c9a4223103e142dbbf19a8668543e95da35bb
tgnetok <vera.iva120608@gmail.com>
2024-07-23 14:26:14 +0000

    whaat iiis thiiiis????

--- lab_1_task_1.py
+++ lab_1_task_1.py
@@ -7,21 +7,26 @@ class SchoolJournal :
         self.grade_list = []
 
     def grade (self, grade) :
+
         if grade >= 1 and grade <= 5:
             self.grade_list.append (grade)
 
     def printer (self) :
+
         print (self.subject)
         print (self.student)
         print (self.grade_list)
 
     def final_grade (self) :
+
         final_grade = (sum (self.grade_list)) / (len (self.grade_list))
         print (final_grade)
 
-student_1 = SchoolJournal ('math', 'John')
-student_1.grade (4)
-student_1.grade (3)
-student_1.grade (5)
-student_1.printer ()
-student_1.final_grade ()
+if __name__ == '__main__' :
+    
+    student_1 = SchoolJournal ('math', 'John')
+    student_1.grade (4)
+    student_1.grade (3)
+    student_1.grade (5)
+    student_1.printer ()
+    student_1.final_grade ()
---
+++ lab_1_task_2.py
@@ -0,0 +1,27 @@
+class Pyramid :
+
+    def __init__ (self, max_h, bricks_count = 0) :
+
+        self.max_h = max_h
+        self.bricks_count = bricks_count
+    
+    def add_bricks (self, bricks) :
+        self.bricks_count = self.bricks_count + bricks 
+
+    def get_height (self) :
+        print (self.bricks_count)
+ 
+    def is_done (self, brks = 0) :
+
+        for i in range (0, self.max_h, 1) :
+            brks += 1
+        if self.bricks_count == 0 :
+            print ('Сначала задайте количество кирпичей.')
+        else :
+            procent = (brks / self.bricks_count) * 100
+            print (f'{procent} % сделано.')
+
+
+pyramid_1 = Pyramid (4)
+pyramid_1.add_bricks (6)
+pyramid_1.is_done ()
