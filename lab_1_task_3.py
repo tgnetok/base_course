d4ed153d3fc0b9a2b8a9975d3b8edfb6c0e9c9ba
tgnetok <vera.iva120608@gmail.com>
2024-08-02 12:42:47 +0000

    yay

--- lab_1_task_3.py
+++ lab_1_task_3.py
@@ -1,10 +1,17 @@
 class Puppy :
 
-    states = ['Болеет', 'Выздоравливает', 'Здоров']
+    states = ['Здоров', 'Выздоравливает', 'Болеет']
 
     def __init__ (self, index, state = states[0]) :
 
         self.index = index
         self.state = state
 
-    def get_treatment (self)
+    def get_treatment (self):
+
+        if self.state == states[2] :
+            self.state == states[1]
+        elif self.state == states[1] :
+            self.state == states[0]
+
+puppy_1 = Puppy (1)
