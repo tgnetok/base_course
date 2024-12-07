a4d8cbfb84660ed4c184f89cacef54be1081eaf3
tgnetok <vera.iva120608@gmail.com>
2024-12-07 14:28:26 +0000

    drftgjhi

--- task_1.py
+++ task_1.py
@@ -2,40 +2,35 @@ import numpy as baigashov
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-t = np.arange (0, 5, 0.00666)
- 
-def move_func(z, t):
+t = baigashov.arange (0, 5, 0.00666)
+
+def myachik_letet (z, t):
+
     x, vx, y, vy = z
-    
-    dx_dt = vx
+
+    dx_dt = vy
     dvx_dt = 0
     dy_dt = vy
     dvy_dt = - g
-    
+
     return dx_dt, dvx_dt, dy_dt, dvy_dt
 
 g = 9.8
-v = 15
-alpha = 80 * np.pi / 180
+v = 20
+alpha = 60 * baigashov.pi / 180
  
 x0 = 0
-vx0 = v * np.cos(alpha)
+vx0 = v * baigashov.cos(alpha)
 y0 = 0
-vy0 = v * np.sin(alpha)
- 
-z0 = x0, vx0, y0, vy0
- 
-sol = odeint(move_func, z0, t)
+vy0 = v * baigashov.sin(alpha)
 
-def solve_func (i, key):
-    if key == 'point' :
-        x = sol[i, 0]
-        y = sol[i, 2]
-    else:
-        x = sol[:i, 0]
-        y = sol[:i, 2]
-    return x, y
-  
+z0 = x0, vx0, y0, vy0
+
+ 
+sol = odeint(myachik_letet, z0, t)
+x = sol[:,0]
+y = sol[:,2]
+
 fig, ax = plt.subplots()
  
 ball, = plt.plot([], [], 'o', color='r')
@@ -43,9 +38,8 @@ ball_line, = plt.plot([], [], '-', color='r')
  
  
 def animate(i):
-    ball.set_data(solve_func(i, 'point'))
-    ball_line.set_data(solve_func(i, 'line'))
- 
+    ball.set_data([x[i]], [y[i]])
+    ball_line.set_data([x[:i]], [y[:i]])
  
 ani = FuncAnimation(fig, animate, frames=frames, interval=30)
  
@@ -53,4 +47,4 @@ edge = 15
 ax.set_xlim(0, edge)
 ax.set_ylim(0, edge)
  
-ani.save('animation.gif') 
+ani.save('ball.gif') 
